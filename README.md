7eac8b866ab4261cda7cacda067dc04968a627e2
Pathak Visham <110142@eesd.org>
2021-01-15 00:16:57 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+## Concatenation
+
+`+` Adding strings together, appending (add to the end)
+
+"Hello" + "World" --> "HelloWorld"
+
+## Type casting
+
+Forcing a data type on a variable
+
+### To force an integer into a string
+`str(variableName)`
+
+### To force a string into an integer
+`int(variableName)`
+
+## Input
+
+input is the way the computer gets information from the user
+
+python initially treats inputs as __string__ type
+
+Ex.
+5 --> "5"
+Today is Thursday --> "Today is Thursday"
+
+### Formula :
+`variableName` = input("optionalPrompt")`
+Python stores the __input__ in the __variableName__ 
---
+++ main.py
@@ -0,0 +1,27 @@
+print("Hello"+ " " + "World"+ " today"" is "+"Thursday")
+#this is concatenation
+print("This starts a new line")
+print()
+print("Tomorrow is Friday")
+
+#if adding strings is called "concatenation"
+#what is + for integers called? addition
+#5 + 4 = 9
+
+print("5" + "4") #concatenation
+print(5 + 4) #addition
+
+firstName = "Visham"
+print("my name is " + firstName)
+age = 12 #integer
+print("my age is " + str(age))
+
+#firstName != FirstName
+
+userName = input("What is your name? ")
+print("Hey " + userName)
+
+#ask user for their birth year
+
+userBirth = input("When is your birth year? ")
+print("Wow you are " + str(2021-int(userBirth)) + " years old!")
